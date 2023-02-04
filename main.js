1122352c43ffdfbf03bf02f5759aa4c2d546277e
Elvis <79801444+Mace2021@users.noreply.github.com>
2023-02-03 20:41:16 -0800

    form update

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
-window.onload=Screen;
-;(function($) { // Begin jQuery
-  $(function() { // DOM ready
+window.onload = Screen;
+(function ($) {
+  // Begin jQuery
+  $(function () {
+    // DOM ready
     // If a link has a dropdown, add sub menu toggle.
-    $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').toggle();
+    $("nav ul li a:not(:only-child)").click(function (e) {
+      $(this).siblings(".nav-dropdown").toggle();
       // Close one dropdown when selecting another
-      $('.nav-dropdown').not($(this).siblings()).hide();
+      $(".nav-dropdown").not($(this).siblings()).hide();
       e.stopPropagation();
     });
     // Clicking away from dropdown will remove the dropdown class
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
+    $("html").click(function () {
+      $(".nav-dropdown").hide();
     });
     // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function() {
-      $('nav ul').slideToggle();
+    $("#nav-toggle").click(function () {
+      $("nav ul").slideToggle();
     });
     // Hamburger to X toggle
-    $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active');
+    $("#nav-toggle").on("click", function () {
+      this.classList.toggle("active");
     });
   }); // end DOM ready
 })(jQuery); // end jQuery
 
+document.getElementById("submit").addEventListener("click", myFunction);
+function myFunction() {
+  window.location.replace("/thanks.html");
+}
 
-  var form = document.getElementById('sheetdb-form');
-        form.addEventListener("submit", e => {
-          e.preventDefault();
-          fetch(form.action, {
-              method : "POST",
-              body: new FormData(document.getElementById("sheetdb-form")),
-          }).then(
-              response => response.json()
-          ).then((html) => {
-            // you can put any JS code here
-            window.location.replace('/thanks.html');
+const form = document.querySelector("#contact");
+const submitButton = document.querySelector("#submit");
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyatks-n-sBJmpkFFKfqjXhFDOba0qTsuFXNsB5wRSRdeEes5zGdGCw7OypNHbGwPW-/exec";
 
-          });
-        });
-      
-        
-        
-         document.getElementById("submit").addEventListener("click", myFunction);  
-         function myFunction() {  
-           window.location.href="/thanks.html";  
-         }
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  let requestBody = new FormData(form);
+  fetch(scriptURL, { method: "POST", body: requestBody })
+    .then((response) => {
+      alert("Success!", response);
+      submitButton.disabled = false;
+    })
+    .catch((error) => {
+      alert("Error!", error.message);
+      submitButton.disabled = false;
+    });
+});
