16df64808103ffc7dab54123f641840b66b89d31
Elvis <79801444+Mace2021@users.noreply.github.com>
2023-02-03 21:42:33 -0800

    update

--- main.js
+++ main.js
@@ -27,22 +27,4 @@ window.onload = Screen;
 
 
 
-const form = document.querySelector("#contact");
-const submitButton = document.querySelector("#submit");
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbyz8Lk6aqolmm9S6DGvDV-F_t-OM84ARBqf6PJAaOvE_6TSau52raTgO5ev86swEiRx/exec";
 
-form.addEventListener("submit", (e) => {
-  submitButton.disabled = true;
-  e.preventDefault();
-  let requestBody = new FormData(form);
-  fetch(scriptURL, { method: "POST", body: requestBody })
-    .then((response) => {
-      alert("Success!", response);
-      submitButton.disabled = false;
-    })
-    .catch((error) => {
-      alert("Error!", error.message);
-      submitButton.disabled = false;
-    });
-});
