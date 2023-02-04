6372a7f898628ba3413b85969bf42565866b86a2
Elvis <79801444+Mace2021@users.noreply.github.com>
2023-02-03 22:54:29 -0800

    update

--- main.js
+++ main.js
@@ -23,26 +23,3 @@
   });
 })(jQuery);
 
-const form = document.querySelector("#form");
-const btn = document.querySelector("#submit");
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbwRsVIqpfHc-xPT_rwdzyAgDGJfzt2cd1izsa4U_BwUey0-P3T8X3ZHYVkAGzZkIWBv/exec";
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  btn.disabled = true;
-  btn.innerHTML = "Loading...";
-
-  console.log(form);
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      btn.disabled = false;
-      btn.innerHTML = "Submit";
-      alert("Success!", response);
-    })
-    .catch((error) => {
-      btn.disabled = false;
-      btn.innerHTML = "Submit";
-      alert("Error!", error.message);
-    });
-});
