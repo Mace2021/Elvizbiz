17c4175a8569749bcbbc949d91c79af354a35fc0
Elvis <79801444+Mace2021@users.noreply.github.com>
2023-02-05 16:47:09 -0800

    update

--- main.js
+++ main.js
@@ -22,23 +22,43 @@
     });
   });
 })(jQuery);
+const sheetName = 'Sheet1'
+const scriptProp = PropertiesService.getScriptProperties()
 
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbzAqgCg2i5iDa6QmG67r_uiGRxONFVU7LwqQicBKJUZujitPtBPTGF496lpG7VxUod-/exec";
+function intialSetup () {
+ const activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+ scriptProp.setProperty('key', activeSpreadsheet.getId())
+}
 
-const form = document.querySelector("#form");
-const submitButton = document.querySelector("#submit");
-form.addEventListener("submit", (e) => {
-  submitButton.disabled = true;
-  e.preventDefault();
-  let requestBody = new FormData(form);
-  fetch(scriptURL, { method: "POST", body: requestBody })
-    .then((response) => {
-      alert("Success!", response);
-      submitButton.disabled = false;
-    })
-    .catch((error) => {
-      alert("Error!", error.message);
-      submitButton.disabled = false;
-    });
-});
+function doPost (e) {
+ const lock = LockService.getScriptLock()
+ lock.tryLock(10000)
+
+ try {
+   const doc = SpreadsheetApp.openById(scriptProp.getProperty('key'))
+   const sheet = doc.getSheetByName(sheetName)
+
+   const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0]
+   const nextRow = sheet.getLastRow() + 1
+
+   const newRow = headers.map(function(header) {
+     return header === 'Date' ? new Date() : e.parameter[header]
+   })
+
+   sheet.getRange(nextRow, 1, 1, newRow.length).setValues([newRow])
+
+   return ContentService
+     .createTextOutput(JSON.stringify({ 'result': 'success', 'row': nextRow }))
+     .setMimeType(ContentService.MimeType.JSON)
+ }
+
+ catch (e) {
+   return ContentService
+     .createTextOutput(JSON.stringify({ 'result': 'error', 'error': e }))
+     .setMimeType(ContentService.MimeType.JSON)
+ }
+
+ finally {
+   lock.releaseLock()
+ }
+}
